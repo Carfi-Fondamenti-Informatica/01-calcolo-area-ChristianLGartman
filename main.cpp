2788dfed1c1ae020609539e9c890c80d193e1d10
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-10 01:54:05 +0100

    Update main.cpp
    Ho cercato di curare anche l'aspetto del "dialogo" fra l'utente e la macchina. Se fosse inutile me lo dica pure, al liceo ero abituato anche a valorizzare questo aspetto. Grazie mille.
    Christian L. Gartman

--- main.cpp
+++ main.cpp
@@ -2,11 +2,25 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
 
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a=0, b=0, c=0, A=0;
+    cout<<"Dimmi il valore di a, di b e di c"<< std::endl;
+    cin >> a >> b >> c;
+    if(a>0 && b>0 && c>0) {
+        cout<<"il valore dell'area del triangolo è: ";
+        A=a*b/2;
+        cout<<A<< std::endl;
+        cout<<"il valore dell'area del quadrato è: ";
+        A=a*a;
+        cout<<A<< std::endl;
+        cout<<"il valore dell'area del rettangolo è: ";
+        A=a*b;
+        cout<<A<< std::endl;
+        cout<<"il valore dell'area del trapezio è: ";
+        A=(a+b)*c/2;
+        cout<<A<< std::endl;
+    }
+    else {
+        cout<<"La lunghezza non può essere negativa o nulla, scrivi valori positivi e maggiori di zero.";
+    }
 }
