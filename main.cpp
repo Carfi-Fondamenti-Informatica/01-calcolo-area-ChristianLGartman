4268af34b7d264490f58375928634cc01bfd76d9
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-10 01:59:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,12 @@ int main(){
     cout<<"Dimmi il valore di a, di b e di c"<< std::endl;
     cin >> a >> b >> c;
     if(a>0 && b>0 && c>0) {
-        cout<<"il valore dell'area del triangolo è: ";
         A=a*b/2;
         cout<<A<< std::endl;
-        cout<<"il valore dell'area del quadrato è: ";
         A=a*a;
         cout<<A<< std::endl;
-        cout<<"il valore dell'area del rettangolo è: ";
         A=a*b;
         cout<<A<< std::endl;
-        cout<<"il valore dell'area del trapezio è: ";
         A=(a+b)*c/2;
         cout<<A<< std::endl;
     }
